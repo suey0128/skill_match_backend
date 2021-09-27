87268fb45ebea7bffd3127deaf4dccb704e8fd48
Joyce Lee <joycelee901@gmail.com>
2021-09-27 18:33:20 -0500

    updated job_seeker and recruiter serializer files to fix errors

--- app/serializers/job_seeker_serializer.rb
+++ app/serializers/job_seeker_serializer.rb
@@ -1,3 +1,3 @@
 class JobSeekerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :username, :password, :email, :image, :profile_id, :skills, :view_skills, :events, :view_events, :recruiters, :all_matching_recruiters, :all_matching_events
+  attributes :id, :name, :location, :username, :password, :email, :image, :profile, :skills, :view_skills, :events, :view_events, :recruiters, :all_matching_recruiters, :all_matching_events
 end
