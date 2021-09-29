f6dcb5835b683f1bf014b457efd63643f8ecc8b2
suey0128 <syui0128@gmail.com>
2021-09-29 02:27:54 -0700

    added :add_events attribute in job seeker serializer for event related frontend function

--- app/serializers/job_seeker_serializer.rb
+++ app/serializers/job_seeker_serializer.rb
@@ -1,3 +1,4 @@
 class JobSeekerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :username, :password, :email, :image, :profile, :skills, :view_skills, :events, :view_events, :recruiters, :all_matching_recruiters, :all_matching_events_for_front_end
+  attributes :id, :name, :location, :username, :password, :email, :image, :profile, :skills, :view_skills, :events, :view_events, :recruiters, 
+    :all_matching_recruiters, :all_matching_events_for_front_end, :add_events
 end
