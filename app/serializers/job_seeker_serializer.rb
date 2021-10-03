36b78fd9d460a611c8eaa9e0bc9c711607c59ccf
suey0128 <syui0128@gmail.com>
2021-10-03 02:21:34 -0700

    added all_matching_recruiters_with_skills method for displaying matching skills in the frontend

--- app/serializers/job_seeker_serializer.rb
+++ app/serializers/job_seeker_serializer.rb
@@ -1,4 +1,4 @@
 class JobSeekerSerializer < ActiveModel::Serializer
   attributes :id, :name, :location, :username, :password, :email, :image, :profile, :skills, :view_skills, :events, :view_events, :recruiters, 
-    :all_matching_recruiters, :all_matching_events_for_front_end, :add_events
+    :all_matching_recruiters, :all_matching_events_for_front_end, :add_events, :all_matching_recruiters_with_skills
 end
